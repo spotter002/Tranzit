ef598619e1b4475d903ead9a90fb45247073d61a
spotter002 <spotterwamai@gmail.com>
2025-08-01 03:36:51 +0300

    Update dashboardRoutes.js

--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -8,12 +8,12 @@ const  DriverDashboard  = require('../controller/driverdash');
 const  {auth , authorizeRoles}  = require('../middleware/auth');
 
 // Admin Dashboard
-router.get('/admin', auth , authorizeRoles('super-admin','admin'), AdminDashboard.getAdminDashboard);
+router.get('/admin', AdminDashboard.getAdminDashboard);
 
 // Shipper Dashboard
-router.get('/shipper',auth , authorizeRoles('admin' ,'super-admin', 'shipper'), ShipperDashboard.getShipperDashboard);
+router.get('/shipper', ShipperDashboard.getShipperDashboard);
 
 // Driver Dashboard
-router.get('/driver', auth, authorizeRoles('admin' , 'super-admin','driver'), DriverDashboard.getDriverDashboard);
+router.get('/driver', DriverDashboard.getDriverDashboard);
 
 module.exports = router;
