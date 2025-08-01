3a4af8a03f2993a88ee6b0514eb6b8c49e39e7d8
spotter002 <spotterwamai@gmail.com>
2025-08-01 03:35:24 +0300

    Update driverRoute.js

--- routes/driverRoute.js
+++ routes/driverRoute.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const driverController = require('../controller/driver')
 const {auth, authorizeRoles} = require('../middleware/auth')
 //driver routes
-router.post('/driver/register',auth, authorizeRoles('admin'), driverController.registerDriver)
+router.post('/driver/register', driverController.registerDriver)
 router.get('/driver', driverController.getDriver)
 router.get('/driver/:id', driverController.getDriverById)
 router.put('/driver/:id',auth, authorizeRoles('admin'), driverController.updateDriver)
