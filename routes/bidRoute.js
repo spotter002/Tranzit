e7155f674569b0b27d6b72cb72f6c79b37610a86
spotter002 <spotterwamai@gmail.com>
2025-08-12 12:06:47 +0300

    Update bidRoute.js

--- routes/bidRoute.js
+++ routes/bidRoute.js
@@ -8,7 +8,7 @@ router.get('/:id', bidController.getBidById);
 router.post('/', bidController.createBid);
 
 router.get('/:id', driverController.getBidsByDriver);
-router.get('/', bidController.getBid);
+router.get('/', bidController.getAllBids);
 router.put('/:id', bidController.updateBid);
 router.delete('/:id', bidController.deleteBid);
 
