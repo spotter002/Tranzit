defa8d0e4219223f3f299634d82ba5c85fd67531
spotter <spotterwamai@gmail.com>
2025-08-15 21:39:39 +0300

    home 004

--- routes/bidRoute.js
+++ routes/bidRoute.js
@@ -2,24 +2,25 @@ const express = require('express');
 const router = express.Router();
 const bidController = require('../controller/bid');
 const driverController = require('../controller/driver')
+const {auth, authorizeRoles} = require('../middleware/auth')
 
 
 // NEW: Get bids for specific job
-router.get('/job/:jobId', bidController.getBidsByJob);
+router.get('/job/:jobId',auth, authorizeRoles('shipper', 'driver', 'admin'), bidController.getBidsByJob);
 
 // NEW: Accept bid and reject others
-router.post('/accept/:bidId', bidController.acceptBid);
+router.post('/accept/:bidId',auth, authorizeRoles('shipper', 'driver', 'admin'), bidController.acceptBid);
 
 
 // ✅ point to the right controller & match param name
-router.get('/driver/:driverId', bidController.getBidsByDriver);
+router.get('/driver/:driverId',auth, authorizeRoles('shipper', 'driver', 'admin'), bidController.getBidsByDriver);
 
-router.get('/', bidController.getAllBids);
-router.post('/', bidController.createBid);
-router.put('/:id', bidController.updateBid);
-router.delete('/:id', bidController.deleteBid);
+router.get('/',auth, authorizeRoles('shipper', 'driver', 'admin'), bidController.getAllBids);
+router.post('/',auth, authorizeRoles('shipper', 'driver', 'admin'), bidController.createBid);
+router.put('/:id',auth, authorizeRoles('shipper', 'driver', 'admin'), bidController.updateBid);
+router.delete('/:id',auth, authorizeRoles('shipper', 'driver', 'admin'), bidController.deleteBid);
 
 
-router.get('/:id', bidController.getBidById);
+router.get('/:id',auth, authorizeRoles('shipper', 'driver', 'admin'), bidController.getBidById);
 
 module.exports = router;
